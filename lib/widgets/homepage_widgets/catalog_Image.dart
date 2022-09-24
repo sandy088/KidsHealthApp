4aeac9826cd9df37ca0d80eff31c9508d4e4c12c
Sandeep <sandeeppakho55@gmail.com>
2022-09-24 16:49:38 +0530

    Remove unwanted import

--- lib/widgets/homepage_widgets/catalog_Image.dart
+++ lib/widgets/homepage_widgets/catalog_Image.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
 
 class CatalogImage extends StatelessWidget {
   final String image;
